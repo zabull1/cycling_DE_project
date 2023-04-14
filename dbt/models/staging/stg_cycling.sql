13e7b0a209f043f4684fb57a73246873a97f7401
AMINU LAWAL <talk2aminulawal@gmail.com>
2023-04-14 17:27:27 +0000

    stg_cycling

--- dbt/models/staging/stg_cycling.sql
+++ dbt/models/staging/stg_cycling.sql
@@ -1,5 +1,31 @@
-{{config(materalized= 'view')}}
+{{ config(materialized="table") }}
 
-select * from {{source('staging', 'cycling_table')}}
+with cycling as (
+    select *,
+        row_number() over (partition by cast(Rental_id as integer), Start_Date) as rn
+    from {{ source("staging", "cycling_table") }}
+    where Rental_id is not null
+)
 
-limit 100
+select 
+    cast(Rental_id as string) as Rental_id,
+    cast(Bike_id  as string) as Bike_id,
+    cast(Start_Date as timestamp) as Start_Date,
+    cast(StartStation_id as string) as StartStation_id,
+    cast(StartStation_Name as string) as StartStation_Name,
+    cast(End_Date as timestamp) as End_Date,
+    cast(EndStation_id as string) as EndStation_id,
+    cast(EndStation_Name as string) as EndStation_Name,
+    cast(Duration as integer) as Duration
+
+from  cycling
+
+where rn =1
+
+{% if var('is_test_run', default=false) %}
+
+  limit 100
+
+{% endif %}
+
+
